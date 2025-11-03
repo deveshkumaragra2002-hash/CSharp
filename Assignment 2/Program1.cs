062063cbbe297ffc2716b49090987c0e1e5db3d8
Devesh kumar <deveshkumaragra2002@gmail.com>
2025-11-03 17:00:39 +0530

    Update usernames and emails for user profiles

--- Assignment 2/Program1.cs
+++ Assignment 2/Program1.cs
@@ -56,20 +56,20 @@ class Program1
     static void Main()
     {
         UserProfile user1 = new UserProfile();
-        user1.SetUsername("Rahul");
+        user1.SetUsername("Devesh");
         user1.SetPassword("12345"); // invalid
         user1.SetPassword("123456"); // valid
-        user1.SetEmail("rahulgmail.com"); // invalid
-        user1.SetEmail("rahul@gmail.com"); // valid
+        user1.SetEmail("deveshgmail.com"); // invalid
+        user1.SetEmail("devesh@gmail.com"); // valid
 
         Console.WriteLine("Username: " + user1.GetUsername());
         Console.WriteLine("Password: " + user1.GetPassword());
         Console.WriteLine("Email: " + user1.GetEmail());
 
         UserProfile user2 = new UserProfile();
-        user2.SetUsername("Amit");
+        user2.SetUsername("Bobby");
         user2.SetPassword("abcdef");
-        user2.SetEmail("amit@example.com");
+        user2.SetEmail("bobby@example.com");
 
         Console.WriteLine("\nUsername: " + user2.GetUsername());
         Console.WriteLine("Password: " + user2.GetPassword());
